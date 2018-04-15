0756653e52bcdd4bd28964abe182ca1dbd66125f
sbmart <sbmart73@gmail.com>
2018-04-15 18:19:56 +0300

    Post object added to schema

--- schema.js
+++ schema.js
@@ -37,3 +37,30 @@ const Person = new GraphQLObjectType({
         }
     }
 });
+
+const Post = new GraphQLObjectType({
+    name: 'Post',
+    description: 'This represents a Post',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(post) {
+                    return post.id;
+                }
+            },
+            title: {
+                type: GraphQLString,
+                resolve(post) {
+                    return post.title;
+                }
+            },  
+            content: {
+                type: GraphQLString,
+                resolve(post) {
+                    return post.content;
+                }
+            }, 
+        }
+    }
+});
