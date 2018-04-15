af9cd91d2505058643acbc7f7014a3d427dcf035
sbmart <sbmart73@gmail.com>
2018-04-15 16:20:27 +0300

    populate person table with Faker

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import _ from 'lodash'
+import Faker from 'faker'
 
 const Conn = new Sequelize(
     'relay',
@@ -25,4 +27,33 @@ const Person = Conn.define('person', {
             isEmail: true
         }
     }
-})
+});
+
+const Post = Conn.define('post', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false  
+    }
+});
+
+// Relate tables:
+Person.hasMany(Post);
+Post.belongsTo(Person);
+
+Conn.sync({force: true}).then(()=>{
+_.times(100, ()=>{
+    return Person.create({
+        firstName: Faker.name.firstName(),
+        lastName: Faker.name.lastName(),
+        // email: Faker.internet.exampleEmail()
+        email: Faker.internet.email()
+    })
+});
+});
+
+export default Conn;
+
