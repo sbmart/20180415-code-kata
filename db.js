e942b8b2220867df617b57532c6bb5ebe65f1447
sbmart <sbmart73@gmail.com>
2018-04-17 20:30:17 +0300

    Update db.js

--- db.js
+++ db.js
@@ -48,6 +48,7 @@ Post.belongsTo(Person);
 // With 14K persons PersonTableSize is 1256kb on pg10
 // lazy GC=>>
 // https://github.com/elastic/kibana/issues/5595
+// https://blog.risingstack.com/finding-a-memory-leak-in-node-js/
 Conn.sync({force: true}).then(()=>{
 _.times(100, ()=>{
     return Person.create({
